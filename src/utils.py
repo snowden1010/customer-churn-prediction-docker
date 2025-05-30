9e45f031a41263d6c38ff237705f56af0a023e69
Abderrahmane Iddaoudi <119764792+snowden1010@users.noreply.github.com>
2025-05-30 21:09:27 +0100

    EDA : downloade kaggle dataset

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,22 @@
-# utils.py (fix typo from utlis.py)
+import os
+from kaggle.api.kaggle_api_extended import KaggleApi
 
-# ...existing code...
+
+
+def download_kaggle_dataset(dataset_name, destination_folder):
+    """
+    Download a Kaggle dataset to the specified destination folder.
+    
+    :param dataset_name: The name of the Kaggle dataset (e.g., 'zillow/zecon').
+    :param destination_folder: The folder where the dataset will be downloaded.
+    """
+    api = KaggleApi()
+    api.authenticate()
+    
+    if not os.path.exists(destination_folder):
+        os.makedirs(destination_folder)
+    
+    api.dataset_download_files(dataset_name, path=destination_folder, unzip=True)
+    print(f"Dataset {dataset_name} downloaded and extracted to {destination_folder}.")
+
+
